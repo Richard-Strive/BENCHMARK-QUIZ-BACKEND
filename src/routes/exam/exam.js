6241a1270e426a46c0cf98e1fdd2bce4f4f76eb4
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-07 14:40:18 +0100

    Work in progress

--- src/routes/exam/exam.js
+++ src/routes/exam/exam.js
@@ -8,9 +8,11 @@ const { write } = require("fs");
  Riferimento path const pathQuiz= join(__dirname,"./quiz.json")  OK
  Riferimento path const pathExam= join(__dirname,"./exam.json")  OK
 
-1. Probabilmente fare il post su un json file solo per l'esame
+1. Probabilmente fare il post su un json file solo per l'esame   OK
 
-2. collegare il post anche al file quiz.json per generare 5 quiz random dal array di domande
+2. collegare il post anche al file quiz.json per generare 5 quiz random dal array di domande OK 
+
+3. Eliminare answers 
 
 */
 
@@ -38,9 +40,14 @@ route.post("/start", async (req, res, next) => {
         questionDB[Math.floor(Math.random() * questionDB.length)],
       ],
     };
+
+    // delete newUserExam.question[0].answers;
+
+    newUserExam.question.map((quiz) => delete quiz.answers);
+
     examDB.push(newUserExam);
     writeFile(examPath, examDB);
-    res.status(201).send("POSTED");
+    res.status(201).send(newUserExam);
   } catch (error) {
     console.log(error);
     next(error);
