dea56a98a39304a9943e9739ed66dbbb03c105c8
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2021-01-07 13:26:07 +0100

    Post seems working but i think something is unclear.

--- src/problematicRoutes/errorHandling.js
+++ src/problematicRoutes/errorHandling.js
@@ -2,7 +2,7 @@ const badRequestHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 400) {
     res.status(400).send(err.msg || "Your are making a bad request.");
   } else {
-    next(error);
+    next(err);
   }
 };
 
@@ -10,7 +10,7 @@ const unauthorizedHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 401) {
     res.status(401).send(err.msg || "UNAUTHORIZED!!!");
   } else {
-    next(error);
+    next(err);
   }
 };
 
@@ -18,7 +18,7 @@ const frobiddenHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 403) {
     res.status(403).send(err.msg || "Forbidden attempt");
   } else {
-    next(error);
+    next(err);
   }
 };
 
@@ -26,7 +26,7 @@ const notFoundHandler = (err, req, res, next) => {
   if (err.httpStatusCode === 404) {
     res.status(404).send(err.msg || "Resource not found!");
   } else {
-    next(error);
+    next(err);
   }
 };
 const catchAllErrorHandler = (err, req, res, next) => {
@@ -35,7 +35,7 @@ const catchAllErrorHandler = (err, req, res, next) => {
       .status(500)
       .send(err.msg || "Generic error occured from the server side");
   } else {
-    next(error);
+    next(err);
   }
 };
 
--- src/routes/exam/exam.js
+++ src/routes/exam/exam.js
@@ -2,23 +2,45 @@ const express = require("express");
 const { join } = require("path");
 const { getFile, writeFile } = require("../../weapons/swords");
 const uniqid = require("uniqid");
+const { write } = require("fs");
 
 /*
- Riferimento path const pathQuiz= join(__dirname,"./quiz.json")
+ Riferimento path const pathQuiz= join(__dirname,"./quiz.json")  OK
+ Riferimento path const pathExam= join(__dirname,"./exam.json")  OK
 
 1. Probabilmente fare il post su un json file solo per l'esame
 
 2. collegare il post anche al file quiz.json per generare 5 quiz random dal array di domande
 
-
 */
 
 const route = express.Router();
 
+const examPath = join(__dirname, "./exam.json");
+const examQuiz = join(__dirname, "./question.json");
+
 route.post("/start", async (req, res, next) => {
   try {
-    console.log("Hello");
-    res.status(200).send("OK /start it's working");
+    const examDB = await getFile(examPath);
+    const questionDB = await getFile(examQuiz);
+    const newUserExam = {
+      id: uniqid(),
+      ...req.body,
+      examDate: new Date(),
+      isComplated: false,
+      name: "Admission Test",
+      totalDuration: 30,
+      question: [
+        questionDB[Math.floor(Math.random() * questionDB.length)],
+        questionDB[Math.floor(Math.random() * questionDB.length)],
+        questionDB[Math.floor(Math.random() * questionDB.length)],
+        questionDB[Math.floor(Math.random() * questionDB.length)],
+        questionDB[Math.floor(Math.random() * questionDB.length)],
+      ],
+    };
+    examDB.push(newUserExam);
+    writeFile(examPath, examDB);
+    res.status(201).send("POSTED");
   } catch (error) {
     console.log(error);
     next(error);
